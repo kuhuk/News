b44623c1753cb2d29913ed1e26999753117b597a
Kuhuk Sharma <kuhuk@github.com>
2022-11-11 10:44:12 +0530

    hamburger done

--- app/src/main/java/com/app/news/MainActivity.kt
+++ app/src/main/java/com/app/news/MainActivity.kt
@@ -3,11 +3,19 @@ package com.app.news
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handleHamburgerClicks()
+    }
+
+    private fun handleHamburgerClicks() {
+        img_hamburger.setOnClickListener { drawer_layout.open() }
     }
 }
