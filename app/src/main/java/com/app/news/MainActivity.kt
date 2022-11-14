5af9a7f0e20c842ba277f4a70620eadcbf7e20dc
Kuhuk Sharma <kuhuk@github.com>
2022-11-14 13:38:41 +0530

    added fragment

--- app/src/main/java/com/app/news/MainActivity.kt
+++ app/src/main/java/com/app/news/MainActivity.kt
@@ -2,6 +2,7 @@ package com.app.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.news.news.NewsFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -13,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         handleHamburgerClicks()
+        startNewsFragment()
     }
 
     private fun handleHamburgerClicks() {
         img_hamburger.setOnClickListener { drawer_layout.open() }
+    }
+
+    private fun startNewsFragment() {
+        val newsFragment = NewsFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.flyt_container_body, newsFragment).commit()
     }
 }
