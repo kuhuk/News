767e8c5c93eecdebc73b4cb7e462d23c7dd47c0c
Kuhuk Sharma <kuhuk@github.com>
2022-11-28 14:26:50 +0530

    new fragments introduced || navigation drawer working

--- app/src/main/java/com/app/news/MainActivity.kt
+++ app/src/main/java/com/app/news/MainActivity.kt
@@ -2,9 +2,12 @@ package com.app.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.news.astronomy.AstronomyFragment
 import com.app.news.news.NewsFragment
+import com.app.news.weather.WeatherFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.navigation_drawer.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 @AndroidEntryPoint
@@ -19,10 +22,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleHamburgerClicks() {
         img_hamburger.setOnClickListener { drawer_layout.open() }
+
+        navItemNewsContainer.setOnClickListener { startNewsFragment() }
+        navItemAstronomyContainer.setOnClickListener { startAstronomyFragment() }
+        navItemWeatherContainer.setOnClickListener { startWeatherFragment() }
     }
 
     private fun startNewsFragment() {
         val newsFragment = NewsFragment()
         supportFragmentManager.beginTransaction().replace(R.id.flyt_container_body, newsFragment).commit()
+        txt_toolbar_title.text = resources.getString(R.string.news)
+        drawer_layout.close()
+    }
+
+    private fun startAstronomyFragment() {
+        val astronomyFragment = AstronomyFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.flyt_container_body, astronomyFragment).commit()
+        txt_toolbar_title.text = resources.getString(R.string.astronomy)
+        drawer_layout.close()
+    }
+
+    private fun startWeatherFragment() {
+        val weatherFragment = WeatherFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.flyt_container_body, weatherFragment).commit()
+        txt_toolbar_title.text = resources.getString(R.string.weather)
+        drawer_layout.close()
     }
 }
---
+++ app/src/main/java/com/app/news/astronomy/AstronomyFragment.kt
@@ -0,0 +1,24 @@
+package com.app.news.astronomy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.news.R
+
+class AstronomyFragment : Fragment() {
+
+    companion object {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_astronomy, container, false)
+    }
+}
