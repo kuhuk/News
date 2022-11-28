8cc2d64e82905b7c21faf26cc1f9c1189c21f9c8
Kuhuk Sharma <kuhuk@github.com>
2022-11-28 14:04:50 +0530

    opening detail url in chrome custom tabs from detail fragment

--- app/src/main/java/com/app/news/newsDetail/NewsDetailFragment.kt
+++ app/src/main/java/com/app/news/newsDetail/NewsDetailFragment.kt
@@ -1,12 +1,13 @@
 package com.app.news.newsDetail
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
 import com.app.news.R
-import dagger.hilt.android.AndroidEntryPoint
 
 class NewsDetailFragment : Fragment() {
 
@@ -18,11 +19,23 @@ class NewsDetailFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_detail, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_news_detail, container, false)
+
+        processBundleData()
+        return view
+    }
+
+    private fun processBundleData() {
+        if (arguments != null) {
+            // this url contains data of news --open it in chrome custom tabs
+            val url = requireArguments().get("url")
+
+            val builder = CustomTabsIntent.Builder()
+            builder.setShowTitle(true)
+            val customTabsIntent : CustomTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(url.toString()))
+        }
     }
 }
